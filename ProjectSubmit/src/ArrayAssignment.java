83421777688b9571a63d099e09b8d396277cd0d6
sorikikikim <sorikim98@naver.com>
2021-09-13 12:21:31 +0900

    assignment1

--- ProjectSubmit/src/ArrayAssignment.java
+++ ProjectSubmit/src/ArrayAssignment.java
@@ -4,47 +4,45 @@ import java.util.Scanner;
 
 public class ArrayAssignment {
 	
-	public static void ReadArray(int [][] array) {
-		Scanner scanner; //scanner는 레퍼런스 변수
+	public static void readArray(int [][] array) { //배열을 입력받아 예외처리를 하는 함수
+		Scanner scanner;
 		scanner = new Scanner(System.in);
 		
 		try {
-			for (int row = 0; row < array.length; row++) {
-				for (int column = 0; column < array[row].length; column++) {
-					array[row][column] = scanner.nextInt();
+			for (int row = 0; row < array.length; row++) { //배열의 행을 array.lenght만큼
+				for (int column = 0; column < array[row].length; column++) { //배열의 열을 array의 각 행.length만큼
+					array[row][column] = scanner.nextInt(); //정수로 입력 받는다.
 				}
 			}
 		}
-		catch (InputMismatchException e) {
+		catch (InputMismatchException e) { //예외처리 1.정수가 아닌 수를 입력받는 경우
 			System.out.println("정수만 입력하세요.");
-			scanner.nextLine();
-			System.exit(1);
+			System.exit(1); //프로세스 종료
 		}
-		catch (NoSuchElementException e) {
+		catch (NoSuchElementException e) { //예외처리 2.배열의 원소의 개수가 16개보다 적은 경우
 			System.out.println("입력 개수가 부족합니다.");
-			scanner.nextLine();
-			System.exit(1);
+			System.exit(1); //프로세스 종료
 		}
+		scanner.close();
 	}
 	
-	public static void  TransArray(int [][] array) {
+	public static void  transformArray(int [][] array) { //입력받은 배열을 이진 배열로 변형하는 함수
 		for (int row = 0; row < array.length; row++) {
 			for (int column = 0; column < array[row].length; column++) {
-				if (array[row][column] < 5) {
-					array[row][column] = 0;
-				}
-				else
-					array[row][column] = 1;
+				if (array[row][column] < 5) //배열의 원소가 5보다 작은 경우
+					array[row][column] = 0; //0으로 치환
+				else //배열의 원소가 5보다 같거나 큰 경우
+					array[row][column] = 1; //1로 치환
 			}
 		}
 	}
 	
-	public static void PrintArray(int [][] array) {
+	public static void printArray(int [][] array) { //배열을 출력하는 함수
 		for (int row = 0; row < array.length; row++) {
 			for (int column = 0; column < array[row].length; column++) {
-				System.out.print(array[row][column] + " ");
+				System.out.print(array[row][column] + " "); //배열의 각 원소 출력
 			}
-			System.out.println();
+			System.out.println(); //한 행마다 개행을 해준다.
 		}
 	}
 
@@ -53,10 +51,12 @@ public class ArrayAssignment {
 		src = new int [4][4];
 		
 		System.out.println("한 줄에 4개씩 총 16개의 양수를 입력하세요.");
-		ReadArray(src);
-		TransArray(src);
+		readArray(src);
+		transformArray(src);
 		System.out.println("변형한 binary 배열을 출력합니다.");
-		PrintArray(src);
+		printArray(src);
 	}
-
 }
+
+
+
--- ProjectSubmit/src/chapter3/RockScissorsPaper.java
+++ ProjectSubmit/src/chapter3/RockScissorsPaper.java
@@ -1,10 +1,56 @@
 package chapter3;
+import java.util.Scanner;
 
 public class RockScissorsPaper {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void doGame(String[] rockScissorsPaper) {
+		Scanner scanner = new Scanner(System.in); //입력을 받아 변환하여 반환시켜주는 scanner를 선언하고 할당한다.
+		
+		while (true) { //반복문 실행.
+			int computerInput = (int)(Math.random()*3); //컴퓨터의 입력(내는 값)은 랜덤으로 선택한다.
+			System.out.print("가위 바위 보! >> ");
+			String userInput = scanner.next(); //사용자의 입력(내는 값)은 scanner로 키를 읽어 문자열로 반환한다.
+			
+			if (userInput.equals("그만")) { //사용자가 그만이라고 입력하면 게임을 끝낸다. 가나a
+				System.out.println("게임을 종료합니다...");
+				break; //while문을 빠져나간다.
+			}
+			else if (userInput.equals("가위")) { //사용자가 가위를 내는 경우
+				System.out.print("사용자 = " + userInput + ", 컴퓨터 = " + rockScissorsPaper[computerInput] + ", ");
+				if (rockScissorsPaper[computerInput].equals("가위")) //컴퓨터가 가위를 내는 경우
+					System.out.println("비겼습니다.");
+				else if (rockScissorsPaper[computerInput].equals("바위")) //컴퓨터가 바위를 내는 경우
+					System.out.println("컴퓨터가 이겼습니다.");
+				else if (rockScissorsPaper[computerInput].equals("보")) //컴퓨터가 보를 내는 경우
+					System.out.println("사용자가 이겼습니다.");
+			}
+			else if (userInput.equals("바위")) { //사용자가 바위를 내는 경우
+				System.out.print("사용자 = " + userInput + ", 컴퓨터 = " + rockScissorsPaper[computerInput] + ", ");
+				if (rockScissorsPaper[computerInput].equals("가위")) //컴퓨터가 가위를 내는 경우
+					System.out.println("사용자가 이겼습니다.");
+				else if (rockScissorsPaper[computerInput].equals("바위"))//컴퓨터가 바위를 내는 경우
+					System.out.println("비겼습니다.");
+				else if (rockScissorsPaper[computerInput].equals("보")) //컴퓨터가 보를 내는 경우
+					System.out.println("컴퓨터가 이겼습니다.");
+			}
+			else if (userInput.equals("보")) {
+				System.out.print("사용자 = " + userInput + ", 컴퓨터 = " + rockScissorsPaper[computerInput] + ", ");
+				if (rockScissorsPaper[computerInput].equals("가위")) //컴퓨터가 가위를 내는 경우
+					System.out.println("컴퓨터가 이겼습니다.");
+				else if (rockScissorsPaper[computerInput].equals("바위")) //컴퓨터가 바위를 내는 경우
+					System.out.println("사용자가 이겼습니다.");
+				else if (rockScissorsPaper[computerInput].equals("보")) //컴퓨터가 보를 내는 경우
+					System.out.println("비겼습니다.");
+			}
+			else //가위, 바위, 보, 그만 외의 입력이 들어오는 경우
+				System.out.println("잘못된 입력입니다. 다시 입력하세요.");
+		}
+		scanner.close();
 	}
-
+	
+	public static void main(String[] args) {
+		System.out.println("컴퓨터와 가위 바위 보 게임을 합니다.");
+		String str[] = {"가위", "바위", "보"};
+		doGame(str);
+	}
 }
+
